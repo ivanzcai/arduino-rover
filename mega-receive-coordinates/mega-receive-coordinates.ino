eab69e6c10e468cf37c061b24627851a8f2f44ab
ivan_z_cai <ivan.c@geoop.com>
2026-03-26 00:05:19 +1100

    Signed-off-by: ivan_z_cai <ivan.c@geoop.com>

--- mega-receive-coordinates/mega-receive-coordinates.ino
+++ mega-receive-coordinates/mega-receive-coordinates.ino
@@ -6,7 +6,7 @@
 
 Servo myServo;  // Create a servo object
 
-RF24 radio(9, 10); // CE, CSN (Mega pins: 9, 10)
+RF24 radio(9, 10);  // CE, CSN (Mega pins: 9, 10)
 
 const byte address[6] = "00001";
 char receivedMsg[32];
@@ -19,19 +19,20 @@ int buttonVal = 0;
 void setup() {
   myServo.attach(8);  // Tell the Arduino the servo is on P
   Serial.begin(9600);
-  
+
   if (!radio.begin()) {
     Serial.println("Radio init failed!");
-    while(1);
+    while (1)
+      ;
   }
-  
+
   radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_250KBPS);
   radio.enableAckPayload();  // Must enable on both sides
-  
+
   radio.openReadingPipe(0, address);
   radio.startListening();
-  
+
   Serial.println("Mega Receiver Ready");
 }
 
@@ -42,17 +43,20 @@ void loop() {
     radio.read(&receivedMsg, sizeof(receivedMsg));
     Serial.print("Received: ");
     Serial.println(receivedMsg);
-    
+
     char textIn[32] = "";
     // int x, y, b;
     int parsed = sscanf(receivedMsg, "%d,%d,%d", &xVal, &yVal, &buttonVal);
-       if (parsed == 3) {
+    if (parsed == 3) {
       Serial.print(" -> Parsed X: ");
       Serial.print(xVal);
       Serial.print(" Y: ");
       Serial.print(yVal);
       Serial.print(" Button: ");
       Serial.println(buttonVal);
+      int servoAngle = map(xVal, 0, 1023, 40, 140);
+      myServo.write(servoAngle);
+
     } else {
       Serial.print(" -> Parse failed, got ");
       Serial.print(parsed);
@@ -63,6 +67,6 @@ void loop() {
     const char reply[] = "Hi Nano!";
     radio.writeAckPayload(0, &reply, sizeof(reply));
     Serial.println("ACK payload loaded for next reply");
-    delay(2000);
+    delay(100);
   }
 }
